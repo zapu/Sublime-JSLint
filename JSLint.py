4558caafd6a8f88953ed6f982caf59b0b30dc38e
Michal Zochniak <michal@zapu.net>
2014-05-01 20:08:28 +0200

    coffeescript linter support. requires coffeelint (npm install -g coffeelint).

--- JSLint.py
+++ JSLint.py
@@ -11,15 +11,25 @@ SETTINGS_FILE = 'JSLint.sublime-settings'
 class JsLintExecCommand(sublime_plugin.WindowCommand):
 
 	def run(self, files=[]):
+		if len(files) != 1:
+			raise "oops"
+
 		settings = sublime.load_settings(SETTINGS_FILE)
-		self.window.run_command('exec', {
-			'cmd':
-				map(os.path.expanduser, settings.get('jslint', ['node', sublime.packages_path() + '/JSLint/linter.js']))  +
-				settings.get('options', []) +
-				files,
-			'line_regex': settings.get('line_regex', '.*// Line ([0-9]*), Pos ([0-9]*)$'),
-			'file_regex': settings.get('file_regex', '(^[^# ]+.*$)')
-		})
+		if re.search(settings.get('filename_filter')['js'], files[0]):
+			self.window.run_command('exec', {
+				'cmd':
+					map(os.path.expanduser, settings.get('jslint', ['node', sublime.packages_path() + '/JSLint/linter.js']))  +
+					settings.get('options')['js'] +
+					files,
+				'line_regex': settings.get('line_regex', '.*// Line ([0-9]*), Pos ([0-9]*)$'),
+				'file_regex': settings.get('file_regex', '(^[^# ]+.*$)')
+			})
+		elif re.search(settings.get('filename_filter')['coffee'], files[0]):
+			self.window.run_command('exec', {
+				'cmd': settings.get('coffeelint', ['coffeelint']) + files,
+				'line_regex': settings.get('line_regex', ''),
+				'file_regex': settings.get('file_regex', '(^[^# ]+.*$)')
+			})
 
 
 class JsLintOnSave(sublime_plugin.EventListener):
@@ -28,9 +38,12 @@ class JsLintOnSave(sublime_plugin.EventListener):
 		settings = sublime.load_settings(SETTINGS_FILE)
 		if settings.get('run_on_save', False) == False:
 			return
-		if re.search(settings.get('filename_filter'), view.file_name()):
+		filename = view.file_name()
+
+		if (re.search(settings.get('filename_filter')['js'], filename) or
+			re.search(settings.get('filename_filter')['coffee'], filename)):
 			view.window().run_command(EXEC_LINT, {
-				'files': [view.file_name()]
+				'files': [filename]
 			})
 
 
